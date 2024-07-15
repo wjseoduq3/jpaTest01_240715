2425e77bcb7aa586ea2630c0eb4013aca3a7d7f7
wjseoduq2 <dragon65@naver.com>
2024-07-15 15:20:23 +0900

    1'st commit

--- src/test/java/com/jdy/jpatest/ItemRepositoryTest.java
+++ src/test/java/com/jdy/jpatest/ItemRepositoryTest.java
@@ -19,24 +19,27 @@ public class ItemRepositoryTest {
     @DisplayName("상품 저장 테스트")
     public void createItemTest() {
 
-        Item item = new Item();
-        item.setItemNm("테스트 상품");
-        item.setPrice(10000);
-        item.setItemDetail("테슽 상품 상세 설명");
-        item.setStockNumber(100);
-        item.setRegTime(LocalDateTime.now());
-        item.setUpdateTime(LocalDateTime.now());
+        for(int i=1;i<=10;i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품"+i);
+            item.setPrice(10000+i);
+            item.setItemDetail("테슽 상품 상세 설명"+i);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
 
-        Item saveItem = itemRepository.save(item); //insert 문
-        System.out.println(saveItem.toString());
+            Item saveItem = itemRepository.save(item); //insert 문
+            System.out.println(saveItem.toString());
+        }
+
+
     }
 
     @Test
     @DisplayName("상품명 조회")
     public void findByItemNmTest() {
 
-
-
+        Item item = itemRepository.findById(3);
     }
 
 
